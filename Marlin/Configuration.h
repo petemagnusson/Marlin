f2985bf98c0d9e33151ce85eeae2c9e09469e6ca
Pete <pete.magnusson@gmail.com>
2020-10-09 14:23:46 +0200

    extrution lenght
    Updated the extrution length for be able to push 200 mm of filament thought

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -573,7 +573,7 @@
  * Note: For Bowden Extruders make this large enough to allow load/unload.
  */
 #define PREVENT_LENGTHY_EXTRUDE
-#define EXTRUDE_MAXLENGTH 10
+#define EXTRUDE_MAXLENGTH 200
 // 600 default //
 
 //===========================================================================
