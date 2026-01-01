32c0f7f19cd8eacae60f98bee58f7bc594663f10
Pete <pete.magnusson@gmail.com>
2026-01-01 14:06:40 +0100

    20260101
    Working Marlin

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1652,7 +1652,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 0, -48, -0.980 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, 48, -0.980 }
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
@@ -1662,7 +1662,7 @@
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 30
+#define PROBING_MARGIN 10
 
 // X and Y axis travel speed between probes.
 // Leave undefined to use the average of the current XY homing feedrate.
@@ -2357,8 +2357,8 @@
 #define Z_SAFE_HOMING
 
 #if ENABLED(Z_SAFE_HOMING)
-  #define Z_SAFE_HOMING_X_POINT X_CENTER  // (mm) X point for Z homing
-  #define Z_SAFE_HOMING_Y_POINT Y_CENTER  // (mm) Y point for Z homing
+  #define Z_SAFE_HOMING_X_POINT (X_BED_SIZE -10) / 2  // (mm) X point for Z homing
+  #define Z_SAFE_HOMING_Y_POINT (Y_BED_SIZE -10) / 2  // (mm) Y point for Z homing
   //#define Z_SAFE_HOMING_POINT_ABSOLUTE  // Ignore home offsets (M206) for Z homing position
 #endif
 
